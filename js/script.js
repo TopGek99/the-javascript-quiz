76425e1cfc72a78311da3aafa5ff63b8ade23a21
TopGek99 <arowe890@gmail.com>
2021-01-08 23:56:52 +1030

    added comments to javascript and cleaned up code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 // initialising variables that contain (most of) the elements in the HTML file
 var hsLink = document.getElementById("high-score-link");
-var timeDiv = document.getElementById("timer");
+var timeDiv = document.getElementById("time");
 var homeDiv = document.getElementById("home");
 var quizDiv = document.getElementById("quiz");
 var endDiv = document.getElementById("endgame");
 var hsDiv = document.getElementById("high-scores");
-var purpleButtons = document.getElementsByClassName("pbtn");
 var startButton = document.getElementById("start");
 var questionH1 = document.getElementById("question");
 var ansButtons = document.getElementsByClassName("answers");
@@ -16,6 +15,7 @@ var scoreList = document.getElementById("score-list");
 var goBackButton = document.getElementById("go-back");
 var clearButton = document.getElementById("clear");
 
+// initialsing array of question objects, containing a question string, an array of strings for the possible answers, and an integer representing the index of the correct answer
 var questionList = [
     {
         question: "Inside which HTML element do we put the JavaScript?",
@@ -62,14 +62,19 @@ var questionList = [
         correct: 0
     }
 ];
+
+// declaring global variables: qNum representing which of the nine questions is active, with the other two being fairly self-explanatory
 var qNum = 0;
-var timer;
-var interval;
+var timer, interval;
+var scoreArray = [];
+
+// startQuiz function called when the start button, sets a timer for 90 seconds for the duration of the quiz, removes the home screen from sight and shows the questions
 function startQuiz() {
     timer = 91;
     timeDiv.textContent = "Time: "+--timer;
     interval = setInterval(function() {
         timeDiv.textContent = "Time: "+--timer;
+        // smaller interval used to determine (seemingly) instantaneously if the timer has been brought below zero by an incorrectness penalty (-15s)
         var smallInterval = setInterval(function() {
             if (timer < 0) {
                 timer = 0;
@@ -80,9 +85,14 @@ function startQuiz() {
     }, 1000);
     homeDiv.style.display = "none";
     quizDiv.style.display = "block";
+    // note: the parameter for showQuestion here is arbitrary as it is only used if a question has just been answered
+    showQuestion(0);
 }
-var response, colour; 
+
+// showQuestion function holds most of the logic for the quiz 
 function showQuestion(index) {
+    // line 94-118 dedicated to showing the user if they are correct or incorrect
+    var response, colour;
     if (qNum !== 0 && index == questionList[qNum-1].correct) {
         response = "Correct!";
         colour = "green";
@@ -90,6 +100,7 @@ function showQuestion(index) {
     } else if (qNum !== 0) {
         response = "Incorrect!";
         colour = "red";
+        // incorrect answer results in 15s penalty
         timer -= 15;
     }
     var hr = document.createElement("hr");
@@ -106,7 +117,7 @@ function showQuestion(index) {
         hr.style.display = "none";
         responseText.style.display = "none";
     }, 1000);
-
+    // if the question number tracker has reached the end of the list, the quiz ends, if not it populates the page with the next question/answers and iterates the tracker
     if (qNum === questionList.length) {
         endQuiz();
     } else {
@@ -118,6 +129,7 @@ function showQuestion(index) {
     }
 }
 
+// endQuiz function simply removes the question portion of the webpage and shows the "end game" page with the users score, and resets the question number tracker for the next attempt
 function endQuiz() {
     timeDiv.textContent = "Time: "+timer;
     scoreSpan.innerText = timer+".";
@@ -127,13 +139,22 @@ function endQuiz() {
     clearInterval(interval);
 }
 
-startButton.addEventListener("click",startQuiz);
-purpleButtons[0].addEventListener("click",function() { showQuestion(0) });
-purpleButtons[1].addEventListener("click",function() { showQuestion(0) });
-purpleButtons[2].addEventListener("click",function() { showQuestion(1) });
-purpleButtons[3].addEventListener("click",function() { showQuestion(2) });
-purpleButtons[4].addEventListener("click",function() { showQuestion(3) });
+// function to remove the high scores list
+function clearScores() {
+    while (scoreList.hasChildNodes()) {  
+        scoreList.removeChild(scoreList.firstChild);
+    }
+}
 
+startButton.addEventListener("click",startQuiz);
+
+// Could clearly use a loop here but for some reason does not seem to work when I do and I can't for the life of me figure out why, but this works so I'm keeping it
+ansButtons[0].addEventListener("click",function() { showQuestion(0) });
+ansButtons[1].addEventListener("click",function() { showQuestion(1) });
+ansButtons[2].addEventListener("click",function() { showQuestion(2) });
+ansButtons[3].addEventListener("click",function() { showQuestion(3) });
+
+// if the "View Highscores" link is clicked it hides whatever is showing and shows the high scores list
 hsLink.addEventListener("click",function(event) {
     event.preventDefault();
     homeDiv.style.display = "none";
@@ -144,20 +165,40 @@ hsLink.addEventListener("click",function(event) {
     hsDiv.style.display = "block";
 });
 
-
+// when submit button is clicked players score is added to the high score list, and shows the high scores page
 submitButton.addEventListener("click",function() {
     endDiv.style.display = "none";
     hsLink.style.display = "none";
     timeDiv.style.display = "none";
     hsDiv.style.display = "block";
-    var scoreItem = document.createElement("li");
-    scoreItem.textContent = initialsInput.value + " - " + timer;
-    scoreList.appendChild(scoreItem);
-    if ((document.getElementsByTagName("li").length % 2) !== 0) {
-        scoreItem.style.backgroundColor = "lavender";
+    if (scoreArray.length === 0) {
+        scoreArray.push({
+            initials: initialsInput.value,
+            score: timer
+        });
+    } else {
+        for (var i=0;i<scoreArray.length;i++) {
+            if (timer > scoreArray[i].score) {
+                scoreArray.splice(i,0,{
+                    initials: initialsInput.value,
+                    score: timer
+                });
+                break;
+            }
+        }
+    }
+    clearScores();
+    for (var i=0;i<scoreArray.length;i++) {
+        var scoreItem = document.createElement("li");
+        scoreItem.textContent = scoreArray[i].initials + " - " + scoreArray[i].score;
+        scoreList.appendChild(scoreItem);
+        if ((i % 2) === 0) {
+            scoreItem.style.backgroundColor = "lavender";
+        }
     }
 });
 
+// go back button simply resets to home page
 goBackButton.addEventListener("click",function() {
     hsDiv.style.display = "none";
     hsLink.style.display = "block";
@@ -166,9 +207,4 @@ goBackButton.addEventListener("click",function() {
     homeDiv.style.display = "block";
 });
 
-
-clearButton.addEventListener("click", function() {
-    while (scoreList.hasChildNodes()) {  
-        scoreList.removeChild(scoreList.firstChild);
-    }
-});
+clearButton.addEventListener("click", clearScores);
